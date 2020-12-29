ee84e10bf5e364c778b1509311e08b4943074a7d
jzhong10 <junzhong2003@gmail.com>
2020-12-28 23:07:18 -0500

    PigLatin II up and running

--- PigLatin.java
+++ PigLatin.java
@@ -9,7 +9,26 @@ public class PigLatin {
 
       //If you want to read the input word by word
       //this can be replaced with hasNext() and next()
-      System.out.println(pigLatinSimple(args[0]));
+    /*
+      if (args.length>0) {
+        System.out.println(pigLatinSimple(args[0]));
+      }
+*/
+      System.out.println(pigLatin(args[0]));
+  }
+
+  public static String pigLatin(String str) {
+    String[] digraphs = new String[] {"bl", "br", "ch", "ck", "cl", "cr",
+    "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc",
+    "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh",
+     "wr"};
+     String retstr = "";
+    if (str.length()>= 2 && in(str.substring(0, 2), digraphs)) {
+      retstr = str.substring(2, str.length()) + str.substring(0,2) + "ay";
+    } else {
+      retstr = pigLatinSimple(str);
+    }
+    return retstr.toLowerCase();
   }
 
   public static String pigLatinSimple(String str) {
@@ -26,6 +45,15 @@ public class PigLatin {
   public static boolean in(char ch, String[] arrstr) {
     for (int i = 0; i<arrstr.length; i++) {
       if (arrstr[i].charAt(0)==ch) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public static boolean in(String str, String[] arrstr) {
+    for (int i = 0; i<arrstr.length; i++) {
+      if (arrstr[i].equals(str.substring(0,2))) {
         return true;
       }
     }
