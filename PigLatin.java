014b75e8271b82a01fe24b76f80a5a2f0dff6085
jzhong10 <junzhong2003@gmail.com>
2020-12-28 22:54:09 -0500

    PigLatin1 tested to work

---
+++ PigLatin.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class PigLatin {
+  public static void main(String[] args) {
+      //use the standard input (terminal input)
+      //as the string that you read from
+      Scanner n = new Scanner(System.in);
+      //use hasNextLine()/nextLine() to loop over
+      //all of the data
+
+      //If you want to read the input word by word
+      //this can be replaced with hasNext() and next()
+      System.out.println(pigLatinSimple(args[0]));
+  }
+
+  public static String pigLatinSimple(String str) {
+    String[] vowels = new String[] {"a", "e", "i", "o", "u"};
+    if (in(str.charAt(0), vowels)) {
+      return str + "hay";
+    }
+    return str.substring(1, str.length()) + str.charAt(0) + "ay";
+  }
+
+  public static boolean in(char ch, String[] arrstr) {
+    for (int i = 0; i<arrstr.length; i++) {
+      if (arrstr[i].charAt(0)==ch) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
