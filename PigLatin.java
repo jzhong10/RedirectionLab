50af2bd1bd2c860a3b4fa38626a6940cfcf27326
jzhong10 <junzhong2003@gmail.com>
2020-12-28 23:59:12 -0500

    adjusted lowercasing for PigLatin inputs and tested on sonnet.txt

--- PigLatin.java
+++ PigLatin.java
@@ -1,34 +1,41 @@
 import java.util.*;
 public class PigLatin {
   public static void main(String[] args) {
-      //use the standard input (terminal input)
-      //as the string that you read from
-      Scanner n = new Scanner(System.in);
-      //use hasNextLine()/nextLine() to loop over
-      //all of the data
+    //use the standard input (terminal input)
+    //as the string that you read from
+    //use hasNextLine()/nextLine() to loop over
+    //all of the data
 
-      //If you want to read the input word by word
-      //this can be replaced with hasNext() and next()
-    /*
-      if (args.length>0) {
-        System.out.println(pigLatinSimple(args[0]));
-      }
+    //If you want to read the input word by word
+    //this can be replaced with hasNext() and next()
+  /*
+    if (args.length>0) {
+      System.out.println(pigLatinSimple(args[0]));
+    }
 */
-      //System.out.println(pigLatin(args[0]));
-      //System.out.println(pigLatinBest(args[0]));
-      /*
-      System.out.println(pigLatinBest("*emu"));
-      System.out.println(pigLatinBest("4chan"));
-      System.out.println(pigLatinBest("fish!"));
-      System.out.println(pigLatinBest("fish"));
-      System.out.println(pigLatinBest("the."));
-      System.out.println(pigLatinBest("cat!"));
-      System.out.println(pigLatinBest("amazing?"));
-      System.out.println(pigLatinBest("apple%"));
-      */
+    //System.out.println(pigLatin(args[0]));
+    //System.out.println(pigLatinBest(args[0]));
+
+    Scanner n = new Scanner(System.in);
+    String retstr = "";
+    while (n.hasNextLine()) {
+      String line = n.nextLine();
+      Scanner currentline = new Scanner(line);
+      while (currentline.hasNext()) {
+        String pigLatined = pigLatinBest(currentline.next());
+        retstr += pigLatined;
+        if (currentline.hasNext()) {
+          retstr += " ";
+        }
+      }
+
+      retstr += "\n";
+    }
+    //System.out.println(retstr);
   }
 
   public static String pigLatinBest(String str) {
+    str = str.toLowerCase();
     String letters = "abcdefghijklmnopqrstuvwxyz";
     String numbers = "1234567890";
     //first character is not a letter
@@ -48,11 +55,12 @@ public class PigLatin {
   }
 
   public static String pigLatin(String str) {
+    str = str.toLowerCase();
     String[] digraphs = new String[] {"bl", "br", "ch", "ck", "cl", "cr",
     "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc",
     "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh",
      "wr"};
-     String retstr = "";
+    String retstr = "";
     if (str.length()>= 2 && in(str.substring(0, 2), digraphs)) {
       retstr = str.substring(2, str.length()) + str.substring(0,2) + "ay";
     } else {
@@ -62,6 +70,7 @@ public class PigLatin {
   }
 
   public static String pigLatinSimple(String str) {
+    str = str.toLowerCase();
     String[] vowels = new String[] {"a", "e", "i", "o", "u"};
     String retstr = "";
     if (in(str.charAt(0), vowels)) {
